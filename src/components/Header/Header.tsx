0847a1f8b491e9295d34b385ff0fb108bc19e00e
RodrigoTissianel <rodrigo.tissianel@outlook.com>
2025-05-28 09:48:48 -0300

    style: adjust header z-index

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderLogo from '../../assets/cheapshark.png';
 const Header = (): React.ReactElement => {
     return (
-        <header className="max-h-20 w-full max-w-7xl mx-auto fixed top-0 z-50 bg-indigo-50 border-b-2 border-b-indigo-100">
+        <header className="max-h-20 w-full max-w-7xl mx-auto fixed top-0 z-10 bg-indigo-50 border-b-2 border-b-indigo-100">
             <nav className="flex justify-start items-center gap-8">
                 <div className="w-14 h-14">
                     <img
