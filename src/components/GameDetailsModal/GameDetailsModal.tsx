eac6300a00fca89c65f4ce7524a4d9b5266144e3
RodrigoTissianel <rodrigo.tissianel@outlook.com>
2025-05-28 00:34:08 -0300

    style: GameDetails modal styles

--- src/components/GameDetailsModal/GameDetailsModal.tsx
+++ src/components/GameDetailsModal/GameDetailsModal.tsx
@@ -1,15 +1,13 @@
+import type { IDeals } from '@/@types/deals';
+import type { IGameDetails } from '@/@types/gameDetails';
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogHeader,
     DialogTitle,
 } from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
-import { useEffect, useState } from 'react';
 import { getGameDetails } from '@/services/api';
-import type { IGameDetails } from '@/@types/gameDetails';
-import type { IDeals } from '@/@types/deals';
+import { useEffect, useState } from 'react';
 import { Skeleton } from '../ui/skeleton';
 
 interface GameDetailsModalProps {
@@ -37,45 +35,63 @@ export const GameDetailsModal = ({
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-            <DialogContent className="max-w-md">
+            <DialogContent
+                className="max-w-md bg-indigo-50 border-indigo-400 border-2"
+                aria-describedby={undefined}
+            >
                 <DialogHeader>
-                    <DialogTitle>{game?.title}</DialogTitle>
-                    <DialogDescription>Informações do jogo</DialogDescription>
+                    {loading ? (
+                        <Skeleton className="w-1/2 h-8 rounded bg-indigo-200" />
+                    ) : (
+                        <DialogTitle className="text-3xl text-indigo-900 font-bold">
+                            {game?.title}
+                        </DialogTitle>
+                    )}
                 </DialogHeader>
                 {loading ? (
-                    <div className="flex flex-col gap-4 mt-4">
-                        <Skeleton className="w-full h-40 rounded bg-gray-200" />
-                        <Skeleton className="h-4 w-3/4 bg-gray-200" />
-                        <Skeleton className="h-4 w-1/2 bg-gray-200" />
+                    <div className="flex flex-col mt-4">
+                        <Skeleton className="w-full h-44 rounded bg-indigo-200" />
+                        <Skeleton className="h-4 w-3/4 mt-2 bg-indigo-200" />
+                        <Skeleton className="h-4 w-1/2 mt-2 bg-indigo-200" />
                         <Skeleton className="h-10 w-full mt-2 rounded" />
                     </div>
                 ) : details ? (
-                    <div className="flex flex-col gap-4 mt-4 text-sm text-indigo-900">
+                    <div className="flex flex-col gap-4 text-sm text-indigo-900">
                         <img
                             src={details.info.thumb}
                             alt={details.info.title}
-                            className="w-full rounded"
+                            className="w-full rounded h-60 object-fill shadow-lg shadow-indigo-400"
                         />
-                        <p>
-                            <strong>Nome:</strong> {details.info.title}
-                        </p>
-                        <p>
-                            <strong>Loja:</strong> {game?.storeName}
-                        </p>
-                        <p>
-                            <strong>Menor preço histórico:</strong> R$
-                            {parseFloat(
-                                details.cheapestPriceEver.price
-                            ).toFixed(2)}
-                        </p>
+                        <div>
+                            <p className="text-[1rem] mt-4">
+                                <strong>Loja:</strong> {game?.storeName}
+                            </p>
+                            <p className="text-[1rem] mt-2">
+                                <strong>Menor preço histórico:</strong> R$
+                                {parseFloat(
+                                    details.cheapestPriceEver.price
+                                ).toFixed(2)}
+                            </p>
+                            <p className="text-[1rem] mt-2">
+                                <strong>Preço original:</strong> R$
+                                {parseFloat(
+                                    details.deals?.[0]?.retailPrice
+                                ).toFixed(2)}
+                            </p>
+                            <p className="text-[1rem] mt-2">
+                                <strong>Preço atual:</strong> R$
+                                {parseFloat(details.deals?.[0]?.price).toFixed(
+                                    2
+                                )}
+                            </p>
+                        </div>
                         <a
                             href={`https://www.cheapshark.com/redirect?dealID=${details.deals?.[0]?.dealID}`}
                             target="_blank"
                             rel="noopener noreferrer"
+                            className="w-1/2 !mx-auto cursor-pointer mt-2 text-center text-[1rem] bg-indigo-800 text-indigo-50 inline-block py-3 rounded-[8px] transition duration-500 hover:opacity-85 shadow-lg hover:shadow-indigo-300"
                         >
-                            <Button className="w-full mt-2">
-                                Ir para oferta
-                            </Button>
+                            Ir para oferta
                         </a>
                     </div>
                 ) : (
